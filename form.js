bc9a411d56d44953197bd862c82b13b8fb2ca0b4
harjuky <35711655+harjuky@users.noreply.github.com>
2018-01-23 07:38:39 +0700

    Create form.js

---
+++ form.js
@@ -0,0 +1,9 @@
+<SCRIPT language = "Javascript">
+function test (){
+var val1=document.harjuky.T1.value
+if (val1%2==0)
+document.harjuky.T2.value = "even number"
+else
+document.harjuky.T2.value = "odd number"
+}
+</SCRIPT>
