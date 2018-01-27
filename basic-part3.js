bf1178a124e3a6d5553117f0feb404f69dd885c3
harjuky <35711655+harjuky@users.noreply.github.com>
2018-01-28 00:28:01 +0700

    Create basic-part3.js

---
+++ basic-part3.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+function count(){
+var utopian_content=document.utopianword.utopianword2.value
+content=utopian_content.split(" ")
+document.utopianword.utopianword3.value=content.length
+}
+</script>
