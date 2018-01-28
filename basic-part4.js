0c1024b9c6dd56348944bf49637b47bfdde1d9db
harjuky <35711655+harjuky@users.noreply.github.com>
2018-01-28 23:07:33 +0700

    Create basic-part4.js

---
+++ basic-part4.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+        function mailcheck(mail){
+        if (mail.indexOf("@")!=-1 && mail.indexOf(".")!=-1)
+        alert("Congratulations !! this is the correct email")
+        else
+        alert("Your email is invalid..")
+        }
+        </script>
